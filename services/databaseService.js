b3510c125a0514d4d2a049ba245d9923d68e7f0c
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-11-03 21:39:01 -0600

    added services for appwrite data fetching

---
+++ services/databaseService.js
@@ -0,0 +1,14 @@
+import { database } from './appwrite'
+
+export const databaseService = {
+  // List Documents
+  async listDocuments(dbId, colId) {
+    try {
+      const response = await database.listDocuments(dbId, colId)
+      return response.documents || []
+    } catch (error) {
+      console.error('Error fetching documents:', error.message)
+      return { error: error.message }
+    }
+  },
+}
